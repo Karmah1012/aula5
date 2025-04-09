e7995d3488ba92dafbfb9827e0dd4743ca80b6ea
Karmah <karmah1012@gmail.com>
2025-04-09 14:50:32 -0300

    esse foi chato, mas rolou

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print()
+num = int(input("Digite o número: "))
+if num > 0 :
+    print (f"Os números são: ")
+    for x in range(1,num+1,1):
+        print(x)
+else:
+        print("Número inválido")
+
+
